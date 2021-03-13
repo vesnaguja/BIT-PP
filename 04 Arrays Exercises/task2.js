bdaf896c47bc4f73286ed9630f11112dff6c4bde
vesnaguja <vesna.grujanic@gmail.com>
2021-03-13 19:34:48 +0100

    Add Arrays Exercises

---
+++ 04 Arrays Exercises/task2.js
@@ -0,0 +1,5 @@
+//2.	Write an arrays of days in a week. Using console.log display Sunday from the array.
+
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+console.log(days[6]);
---
+++ 04 Arrays Exercises/task3.js
@@ -0,0 +1,4 @@
+//3. Print all negative elements from the array [2, -4, 5, -2, -11]. 
+
+var array = [2, -4, 5, -2, -11];
+console.log(array[1], array[3], array[4]);
