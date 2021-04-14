d8bdac82f56b18774050d2bd8c8f5bb43b2240d8
vesnaguja <vesna.grujanic@gmail.com>
2021-04-14 16:27:43 +0200

    IIFE exercise add

---
+++ 13Functions8IIFE/task02.js
@@ -0,0 +1,16 @@
+/*2.	Write IIFE that calculates the surface area of the given rectangle with sides a and b. 
+Input: 4 5
+Output: 20  
+*/
+
+var a = 4;
+var b = 5;
+
+
+(function (a, b) {
+  console.log(a*b);
+  
+})(4,5);
+
+
+
